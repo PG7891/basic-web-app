df7b8cf9467b07144aa85544941547dd2ef85677
Prathik Guduri <prathik@prathiks-mbp-2.wifi.local.cmu.edu>
2024-03-14 15:53:37 -0400

    Adding name functionality

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -7,5 +7,12 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
+  if (query.toLowerCase().includes("what is your name?")) {
+    return (
+      "Prathik"
+    );
+  }
+
+
   return "";
 }
